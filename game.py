a948e9a2197e96b2431e8156b2dfecb1e4aee166
Maria Eduarda <maria.e.rocha23@aluno.senai.br>
2026-02-13 12:04:27 -0300

    barbie

--- game.py
+++ game.py
@@ -1,7 +1,18 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.title = "Game: Adivinhe a Imagem"
+    page.bgcolor="#fc03b6"
+    mensagem = ft.Text("Escolha a opção correta!", color="white")
+    resposta_correta = "Rosa"
+
+    def verificar_resposta(e):
+        if e.control.content == resposta_correta:
+            mensagem.value = "Parabéns, Resposta Correta."
+        else:
+            mensagem.value = "Resposta Incorreta"
+        page.update()
+
+    page.title = "Game: Sapatilha da Barbie."
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
@@ -9,15 +20,41 @@ def main(page: ft.Page):
         ft.Column(
             controls=[
                 ft.Text(
-                    "Adivinhe a Imagem",
+                    "Adivinhe a cor da sapatilha da Barbie.",
+                    color="#fc03b6",
+                    bgcolor="#ffb3e9",
                     size=24,
-                    weight="bold"
+                    weight="bold",
+                    text_align=ft.TextAlign.CENTER
                 ),
                 ft.Image(
-                    src="images/gatinho.jpeg",
+                    src="images/barbie.sap.jpg",
                     height=200
-                )
-            ]
+                ),
+                ft.Row(
+                    controls=[
+                        ft.Button(
+                            content="Rosa",
+                            bgcolor="#fa7ad6",
+                            on_click=verificar_resposta
+                        ),
+                        ft.Button(
+                            content="Roxo",
+                            bgcolor="#cd7afa",
+                            on_click=verificar_resposta
+                        ),
+                        ft.Button(
+                            content="Verde-Água",
+                            bgcolor="#7af1fa",
+                            on_click=verificar_resposta
+                        )
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER
+                ),
+                mensagem
+            ],
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER
+
         )
     )
 
